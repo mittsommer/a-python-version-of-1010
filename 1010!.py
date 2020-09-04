0fe9805cd8518861dd6599a69b6dc3b19c4e1eb3
kai.liang <srabmit.sra.uni-hannover.de>
2020-09-04 23:42:59 +0200

    bug fix

--- 1010!.py
+++ 1010!.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pygame
 import objectlib
+import time
 
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
@@ -22,43 +23,49 @@ class Map():
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
                      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], ]
 
-    def set(self, object, row, line):
+    def canput(self, object, row, line):
         if np.shape(object)[0] + row <= 10 and np.shape(object)[1] + line <= 10:
             pass
         else:
-            print('unable to set here1')
+            #print('unable to set here1')
             return False
         for i in range(len(object)):
             for j in range(len(object[i])):
                 if not (self.netz[row + i][line + j] and object[i][j]):
                     canput = True
                 else:
-                    print('unable to set here2')
-                    canput = False
+                    #print('unable to set here2')
                     return False
-        if canput:
-            for i in range(len(object)):
-                self.netz[row + i][line:line + len(object[i])] += object[i]
+        return canput
 
-        return True
+
+    def set(self,object,row,line):
+        for i in range(len(object)):
+            self.netz[row + i][line:line + len(object[i])] += object[i]
+
+
 
     def check_eliminate(self):
+        eliminate = False
         for i in range(10):
             if self.netz[i] == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]:
-                self.netz[i] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                for j in range(10):
+                    self.netz[i][j] = 0
+                    eliminate = True
         for i in range(10):
             line = [j[i] for j in self.netz]
             if line == [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]:
                 for j in range(10):
                     self.netz[j][i] = 0
-
+                    eliminate = True
+        return eliminate
     def buildmap(self, bg, pinksq, display):
         y = 100
         for i in self.netz:
             x = 100
             for j in i:
                 if j == 0:
-                    display.blit(bg, (x, y))  # 矩阵为0的位置放置背景色块
+                    display.blit(bg, (x, y))# 矩阵为0的位置放置背景色块
                 else:
                     display.blit(pinksq, (x, y))  # 矩阵为1的位置放置颜色色块
                 x = x + 40
@@ -85,6 +92,8 @@ class Square():
 def run_game():
     pinksq = pygame.image.load('image/pink.png')
     pinksq_rect = pinksq.get_rect()
+    gameover = pygame.image.load('image/gameover.png')
+    gameover_rect = gameover.get_rect()
     pygame.init()
     display = pygame.display.set_mode((600, 800))
     pygame.display.set_caption('1010!')
@@ -93,10 +102,20 @@ def run_game():
     sq.Square_gen()
     click = False
     clock = pygame.time.Clock()
+    pickup = False
+
     while True:
         set = False
         display.fill(BLACK)
         map.buildmap(bg, pinksq, display)
+        gamecon = False
+        for row in range(10):
+            for line in range(10):
+                gamecon = map.canput(sq.shape, row, line) or gamecon
+        if not gamecon:
+            gameover_rect.left, gameover_rect.top = 0, 300
+            display.blit(gameover, gameover_rect)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
@@ -105,23 +124,29 @@ def run_game():
                 square = sq.shape
                 if 300 <= x <= 300 + len(square[0]) * 40 and 600 <= y <= 600 + len(square) * 40:
                     click = True
+                    pickup = True
             if event.type == pygame.MOUSEBUTTONUP:
+                x, y = pygame.mouse.get_pos()
+                if not (80 <= x <= 520 and 80 <= y <= 520):
+                    pickup = False
                 click = False
-                set = True
+                if pickup:
+                    set = True
         if set:
             x, y = pygame.mouse.get_pos()
             row = round(10 * (y - 100) / 400)
             line = round(10 * (x - 100) / 400)
-            if map.set(sq.shape, row, line):
+            if map.canput(sq.shape, row, line):
+                map.set(sq.shape, row, line)
                 map.check_eliminate()
                 sq.Square_gen()
         if not click:
             sq.blitsquare(display, (300, 600), pinksq, pinksq_rect)
-        else:
+        elif click:
             x, y = pygame.mouse.get_pos()
             sq.blitsquare(display, (x, y), pinksq, pinksq_rect)
         pygame.display.flip()
-        clock.tick(120)
+        #clock.tick(30)
 
 
 run_game()
